4d4353e617cc4aae9991f0cc9c7acbd3f433f766
Pol d'Avezac <pol.davezac@depixus.com>
2018-01-29 08:57:34 +0100

    removing julia linting as it is too slow

--- _julia.py
+++ _julia.py
@@ -14,25 +14,43 @@ def check_julia(cnf, name, version):
     "checks pylint's astroid version"
     requirements.programversion(cnf, 'julia', version, reg = name)
 
-JULIA_CNT   = itertools.count()
-JULIA_FILE  = str(Path(__file__).parent/"_julialint.jl")
-LINT_SERVER = subprocess.Popen(["julia", JULIA_FILE],
-                               stdout = subprocess.DEVNULL,
-                               stderr = subprocess.DEVNULL)
+class JuliaLint:
+    """
+    Sets up a server for linting julia files
+    """
+    def __init__(self, fname = str(Path(__file__).parent/"_julialint.jl")):
+        self.cnt    = itertools.count()
+        self.file   = fname
+        self.server = None
+
+    def lint(self, bld, src = None):
+        "runs the lint on the file"
+        if src is None:
+            src  = bld.path.ant_glob('**/*.jl')
+
+        if len(src) == 0:
+            return
+
+        if self.server is None:
+            self.server = subprocess.Popen(["julia", self.file],
+                                           stdout = subprocess.DEVNULL,
+                                           stderr = subprocess.DEVNULL)
+        for inp in src:
+            grp = f'julia_{next(self.cnt)}'
+            bld.add_group(grp, move = False)
+            bld(source = [inp],
+                color  = 'BLUE',
+                rule   = f'julia {self.file} ${{SRC}}',
+                group  = grp,
+                cls_keyword = lambda _: 'Julia')
+
+JLINT = JuliaLint()
+
 @conf
 def build_julia(bld:Context, name:str, _:str, **_2):
     "builds a python module"
     if 'julia' not in requirements:
         return
 
-    src  = bld.path.ant_glob('**/*.jl')
-    copyfiles(bld, name, src)
-
-    for inp in src:
-        grp = f'julia_{next(JULIA_CNT)}'
-        bld.add_group(grp, move = False)
-        bld(source = [inp],
-            color  = 'BLUE',
-            rule   = f'julia {JULIA_FILE} ${{SRC}}',
-            group  = grp,
-            cls_keyword = lambda _: 'Julia')
+    copyfiles(bld, name, bld.path.ant_glob("**/*.jl"))
+    #JLINT.lint(bld)
