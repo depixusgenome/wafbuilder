8f5b097c340c970a75efcc79f089ac5fd3995fa1
Pol d'Avezac <pol.davezac@depixus.com>
2019-01-18 10:04:37 +0100

    :refactoring: moving code from trackanalysis to here

---
+++ apppackager.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"Everything related to conda"
+import sys
+import os
+from pathlib    import Path
+from itertools  import chain
+from zipfile    import ZipFile
+from shutil     import rmtree, copy2, move
+from typing     import List
+import py_compile
+
+from waflib.Build   import BuildContext
+
+from .git           import version
+from ._python       import condaenv as _condaenv, condasetup as _condasetup
+
+class AppPackager(BuildContext):
+    "Context for packaging an app"
+    doall         = True
+    excluded      = 'tests', 'scripting'
+    pypath        = "OUTPUT_PY"
+    outpath       = "OUTPUT"
+    libname       = "pylib"
+    resourcepaths: List[str] = []
+    staticdir     = "static"
+    appdir        = "app"
+    codedir       = "code"
+    docdir        = "doc"
+    changelogpath = "CHANGELOG.md"
+
+    def __clean(self):
+        self.options.APP_PATH = self.bldnode.make_node(self.pypath)
+        if self.options.APP_PATH.exists():
+            self.options.APP_PATH.delete()
+
+        self.options.OUT_PATH = self.bldnode.make_node(self.outpath)
+        if self.options.OUT_PATH.exists():
+            self.options.OUT_PATH.delete()
+        self.options.OUT_PATH.mkdir()
+
+    @staticmethod
+    def __compile(path, inp, outp):
+        with open(str(inp), encoding = 'utf-8') as stream:
+            if not any('from_py_func' in i for i in stream):
+                cur = outp/inp.relative_to(path).with_suffix('.pyc')
+                out = str(cur)
+                opt = 0 if 'reporting' in out else 2
+                py_compile.compile(str(inp), out, optimize = opt)
+                return cur
+        return inp
+
+    def __zip_files(self, path, out, zips):
+        with ZipFile(str(out/(self.libname+".pyz")), "w") as zfile:
+            for pyc in path.glob("*.py"):
+                pyc = self.__compile(path, pyc, path)
+                zfile.write(str(pyc), str(pyc.relative_to(path)))
+
+            for mod in zips:
+                for pyc in chain(mod.glob("**/*.pyc"), mod.glob("**/*.py")):
+                    pyc = self.__compile(path, pyc, path)
+                    zfile.write(str(pyc), str(pyc.relative_to(path)))
+
+    def __move_files(self, mods, out, path, dll):
+        for mod in mods:
+            for name in chain(mod.glob('**/*.coffee'), mod.glob("_core"+dll)):
+                outp = out/name.relative_to(path)
+                outp.parent.mkdir(exist_ok = True, parents = True)
+                name.rename(outp)
+
+            for pyc in mod.glob('**/*.py'):
+                outp = self.__compile(path, pyc, out)
+                if outp == pyc:
+                    pyc.rename(out/pyc.relative_to(path))
+
+        for name in path.glob('*'+dll):
+            outp = out/name.relative_to(path)
+            outp.parent.mkdir(exist_ok = True, parents = True)
+            name.rename(outp)
+
+        if (path/self.staticdir).exists():
+            (path/self.staticdir).rename(out/self.staticdir)
+
+        for ext in ("js", "sh", "bat"):
+            for itm in path.glob("*."+ext):
+                itm.rename(out/itm.relative_to(path))
+
+    def __copy_gif(self, path):
+        for res in self.resourcepaths:
+            if res.endswith(".desktop") and not sys.platform.startswith("win"):
+                continue
+            src = self.srcnode.find_resource(res)
+            tgt = path/Path(res).name
+            copy2(src.abspath(), tgt)
+
+    def __final(self, mods):
+        path = Path(str(self.options.APP_PATH))
+        dll  = '.cp*-win*.pyd' if sys.platform.startswith("win") else '.cpython-*.so'
+        mods = [path/Path(mod).name for mod in mods]
+        zips = [
+            mod for mod in mods
+            if (
+                mod.exists() and mod.name != self.appdir
+                and next(mod.glob("_core"+dll), None) is None
+            )
+        ]
+
+        out = Path(str(self.options.OUT_PATH))
+        if len(zips):
+            self.__zip_files(path, out, zips)
+
+        self.__move_files([i for i in mods if i not in zips], out, path, dll)
+
+        final = Path(".")/version()
+        if final.exists():
+            rmtree(str(final))
+        final.mkdir(exist_ok = True, parents = True)
+        final = final / self.codedir
+        os.rename(str(out), str(final))
+        self.__copy_gif(final)
+
+        if (path/self.docdir).exists():
+            move(str(path/self.docdir), str(final.parent))
+
+        if Path(self.changelogpath).exists():
+            out = final.parent/self.changelogpath
+            copy2(self.changelogpath, out)
+            try:
+                os.system("pandoc --toc -s {} -o {}".format(out, out.with_suffix(".html")))
+            except: # pylint: disable=bare-except
+                pass
+
+        for i in list(final.glob("*.bat")) + list(final.glob("*.sh")):
+            os.rename(str(i), str(final.parent/i.name))
+        rmtree(str(path))
+
+    def build_app(self, modules, builder):
+        "builds the app"
+        self.__clean()
+        mods = [i for i in modules(self)
+                if not any(j in i for j in self.excluded)]
+        builder(self, mods)
+        self.recurse(mods, "startscripts", mandatory = False)
+        if self.doall:
+            node = Path(str(self.bldnode.parent))
+            cpy  = Path(str(self.options.OUT_PATH)).relative_to(node)
+            _condasetup(self, copy = str(cpy), runtimeonly = True)
+
+        self.add_group()
+        self(rule = lambda _: self.__final(mods), always = True)
+
+def package(ctxcls, modules, builder, **kwa):
+    """
+    add app packaging functions
+    """
+    funcs       = lambda x, **y: dict({"fun": x, "cmd": x}, **y)
+    _CondaEnv   = type('_CondaEnv', (BuildContext,),  funcs('condaenv'))
+    _CondaSetup = type('_CondaSetup', (ctxcls,),      funcs('setup'))
+    _CondaApp   = type('_CondaPatch', (AppPackager,), funcs('app', doall = True, **kwa))
+    _CondaPatch = type('_CondaPatch', (_CondaApp,),   funcs('apppatch', doall = False))
+
+    def condaenv(cnf):
+        u"prints the conda yaml recipe"
+        modules(cnf)
+        _condaenv(getattr(_CondaApp, 'libname'))
+
+    def setup(cnf):
+        "Sets up the python environment"
+        modules(cnf)
+        _condasetup(cnf)
+        if sys.platform.startswith("win"):
+            print("COFFEESCRIPT is not mandatory & can be installed manually")
+
+    def app(bld):
+        "Creates an application"
+        bld.build_app(modules, builder)
+
+    def apppatch(bld):
+        "Creates an application patch"
+        bld.build_app(modules, builder)
+
+    return dict(
+        _CondaEnv   = _CondaEnv,
+        _CondaApp   = _CondaApp,
+        _CondaPatch = _CondaPatch,
+        _CondaSetup = _CondaSetup,
+        setup       = setup,
+        app         = app,
+        apppatch    = apppatch,
+        condaenv    = condaenv
+    )
---
+++ bokehcompiler.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"Compiles the JS code once and for all"
+import  sys
+from    typing     import List
+from    pathlib    import Path
+import  bokeh.util.compiler as _compiler
+
+sys.path.append(str(Path(".").resolve()))
+def finddependencies(*modules) -> List[str]:
+    "compiles the application as would a normal call to bokeh"
+    for mod in modules:
+        __import__(mod)
+    old = _compiler.nodejs_compile
+    lst = []
+    def _deps(_1, lang="javascript", file=None): # pylint: disable=unused-argument
+        lst.append(file)
+        return _compiler.AttrDict({'code': '', 'deps': []})
+    _compiler.nodejs_compile = _deps
+    _compiler.bundle_all_models()
+    _compiler.nodejs_compile = old
+    return lst
+
+def compileapp(*modules) -> str:
+    "compiles the application as would a normal call to bokeh"
+    for mod in modules:
+        __import__(mod)
+    string = _compiler.bundle_all_models()
+    return f"/*KEY={_compiler.calc_cache_key()}*/\n"+string
+
+class GuiMaker:
+    "make gui"
+    def __init__(self, **kwa):
+        self.modules     = ('taskview.toolbar', 'undo')
+        self.modulepaths = ('core/app', 'core/view')
+        self.docpath     = "doc"
+        self.cmdlines    = (
+            (r'taskapp/cmdline.pyc --port random -g app ',      False),
+            (r'taskapp/cmdline.pyc --port random -g browser ',  True),
+        )
+        self.__dict__.update(kwa)
+
+    def __call__(self, viewname, locs, scriptname = None):
+        "default make for a gui"
+        name = locs['APPNAME'] if scriptname is None else scriptname
+
+        if 'startscripts' not in locs:
+            def startscripts(bld):
+                "creates start scripts"
+                val = r" "+locs['APPNAME']+'.'+viewname
+                for cmd, debug in self.cmdlines:
+                    self.build_startupscripts(bld, name, cmd+val, debug)
+            locs['startscripts'] = startscripts
+
+        old = locs.pop('build')
+        def build(bld):
+            "build gui"
+            old(bld)
+            paths = [bld.bldnode.parent.find_node(i) for i in self.modulepaths]
+            self.build_bokehjs(bld, viewname, list(self.modules), paths, scriptname)
+            self.build_doc(bld, scriptname, self.docpath)
+
+        locs['build'] = build
+
+    @staticmethod
+    def build_startupscripts(bld, name, cmdline, debug = False, path = None):
+        "creates the startup script"
+        if path is None:
+            path = getattr(bld.options, 'APP_PATH', bld.bldnode)
+
+        iswin = sys.platform.startswith("win")
+        ext   = ".bat" if iswin else ".sh"
+        if iswin:
+            cmd = 'cd code\r\n'+r'start /min %~dp0\code\pythonw -I '
+            if debug:
+                cmd = 'cd code\r\n'+r'%~dp0\code\python -I '
+        else:
+            cmd =(r'IR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\n'
+                  r'cd $DIR/code\n'
+                  r'./bin/python')
+
+        fname = str(path.make_node(name+("_debug" if debug else "")+ext))
+        with open(fname, 'w', encoding = 'utf-8') as stream:
+            print(cmd + r" "+ cmdline, file = stream)
+            if debug and iswin:
+                print(r"pause", file = stream)
+
+    @staticmethod
+    def build_bokehjs(bld, viewname, modules, paths, key):
+        "build the js file"
+        if '.' in viewname:
+            for i in viewname.split('.'):
+                if i[0] == i[0].upper():
+                    break
+                modules.append(modules[-1]+'.'+i)
+
+        for i in sum((j.ant_glob('/**/*.py') for j in paths), []):
+            i = i.srcpath()
+            if Path(str(i)).name[:2] != '__':
+                modules.append(str(i)[5:-3].replace("/", ".").replace("\\", "."))
+
+        modules = [i for i in modules if i[:2] != '__']
+
+        root = bld.path.ctx.bldnode
+        mods = [i.split('.')[0] for i in modules]
+        mods = [j for i, j in enumerate(mods) if j not in mods[:i]]
+        srcs = sum((root.ant_glob(i.replace('.', '/')+'/**/*.coffee') for i in mods), [])
+
+        from wafbuilder import copyroot
+        tgt  = copyroot(bld, key+'.js')
+
+        rule = f'{bld.env["PYTHON"][0]} {__file__} '+' '.join(modules)+' -o ${TGT} -k '+key
+        bld(source      = srcs,
+            name        = modules[0]+':bokeh',
+            color       = 'BLUE',
+            rule        = rule,
+            target      = tgt,
+            cls_keyword = lambda _: 'Bokeh',
+            group       = 'bokeh')
+
+    @staticmethod
+    def build_doc(bld, scriptname, path = 'doc'):
+        "create the doc"
+        if not (
+                'SPHINX_BUILD' in bld.env
+                and (Path(str(bld.srcnode))/path/scriptname).exists()
+        ):
+            return
+        if getattr(bld.options, 'APP_PATH', None) is None:
+            target = str(bld.bldnode)+f"/{path}/"+scriptname
+        else:
+            target = str(bld.options.APP_PATH)+f"/{path}/"+scriptname
+
+        rule = (
+            "${SPHINX_BUILD} "+str(bld.srcnode)+f"/{path}/{scriptname} "
+            +"-c "+str(bld.srcnode)+f"/{path} "
+            +target
+            + f" -D master_doc={scriptname} -D project={scriptname} -q"
+        )
+
+        bld(
+            rule   = rule,
+            source = (
+                bld.srcnode.ant_glob(f'{path}/{scriptname}/*.rst')
+                + bld.srcnode.ant_glob(path+'/conf.py')
+            ),
+            target = bld.path.find_or_declare(target+f'/{scriptname}.html')
+        )
+
+    @classmethod
+    def run(cls, viewname, locs, scriptname = None, **kwa):
+        "run this class"
+        cls(**kwa)(viewname, locs, scriptname)
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    import click
+    @click.command()
+    @click.argument('modules', nargs = -1)
+    @click.option("-o", "--output", type = click.Path(), default = None)
+    @click.option("-d", "--dependencies", flag_value = True, default = False)
+    @click.option("-k", "--key", default = "")
+    def _main(modules, output, dependencies, key):
+        if dependencies:
+            string = '\n'.join(finddependencies(*modules))
+        else:
+            string = compileapp(*modules)
+
+        if output is None:
+            print(string)
+        else:
+            if key == "":
+                key = Path(output).stem
+            print(f"/*KEY={key}*/\n"+string,
+                  file = open(output, 'w', encoding='utf-8'))
+
+    _main()
