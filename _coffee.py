9da5fc813afb38dc5e0bc759fedf12cc7973146d
Pol d'Avezac <pol.davezac@depixus.com>
2019-04-07 14:35:49 +0200

    adding coffeescript linting: better output

--- _coffee.py
+++ _coffee.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u"All *basic* coffeescript related details"
+from subprocess         import check_output
 import sys
-
 from pathlib            import Path
-from waflib             import TaskGen
+from waflib             import Logs, Errors, TaskGen
 from waflib.Configure   import conf
 from waflib.Context     import Context
 
@@ -54,18 +54,24 @@ def build_coffee(bld:Context, name:str, _1, **_2):
         if 'coffeelint' not in bld.group_names:
             bld.add_group('coffeelint', move = False)
 
-        cmd = (
-            '${COFFEELINT} --reporter=csv --file=%s ${SRC}'
-            % str(next(
+        def _cmd(tgt, *_):
+            path = str(next(
                 (Path(direct)/'coffeelintrc').resolve()
                 for direct in ('', 'linting', '..', '../linting')
                 if (Path(direct)/'coffeelintrc').exists()
             ))
-        )
+            out = check_output(
+                [bld.env['COFFEELINT'][0], f'--file={path}', '--reporter=csv', str(tgt)]
+            ).strip().split(b'\n')[1:]
+            if out:
+                msg = 'Coffeelint: '+b'\n'.join(out).decode('utf-8')
+                Logs.error(msg)
+                raise Errors.WafError(msg)
+
         for i in coffees:
             bld(
                 source = [i],
-                rule        = cmd,
+                rule        = _cmd,
                 color       = 'BLUE',
                 cls_keyword = lambda _: 'CoffeeLint',
                 group  = 'coffeelint',
