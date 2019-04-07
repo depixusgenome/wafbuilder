20ad8547312ef52e65781146ba9f20e7dd749d12
Pol d'Avezac <pol.davezac@depixus.com>
2019-04-07 13:49:59 +0200

    adding coffeescript linting

--- _coffee.py
+++ _coffee.py
@@ -3,6 +3,7 @@
 u"All *basic* coffeescript related details"
 import sys
 
+from pathlib            import Path
 from waflib             import TaskGen
 from waflib.Configure   import conf
 from waflib.Context     import Context
@@ -25,17 +26,47 @@ def check_coffee(cnf, name, version):
     mand = not sys.platform.startswith("win")
     requirements.programversion(cnf, name, version, mandatory = mand)
 
+@requirements.addcheck
+def check_coffee_coffeelint(cnf, name, version):
+    "check for coffeelint"
+    mand = not sys.platform.startswith("win")
+    requirements.programversion(cnf, name, version, mandatory = mand)
+
 @conf
 def build_coffee(bld:Context, name:str, _1, **_2):
     u"builds all coffee files"
     if 'coffee' not in requirements:
         return
+    if 'COFFEE' not in bld.env or getattr(bld.options, 'APP_PATH', None) is not None:
+        return
+
+    tsx = bld.path.ant_glob('**/*.tsx')
+    copyfiles(bld, name, tsx)
 
     coffees = bld.path.ant_glob('**/*.coffee')
-    if len(coffees):
-        if 'COFFEE' in bld.env and  getattr(bld.options, 'APP_PATH', None) is None:
-            bld(source = coffees)
-        copyfiles(bld, name, coffees)
+    if len(coffees) == 0:
+        return
 
-        tsx = bld.path.ant_glob('**/*.tsx')
-        copyfiles(bld, name, tsx)
+    bld(source = coffees)
+    copyfiles(bld, name, coffees)
+
+    if bld.options.DO_PY_LINTING and 'COFFEELINT' in bld.env:
+        if 'coffeelint' not in bld.group_names:
+            bld.add_group('coffeelint', move = False)
+
+        cmd = (
+            '${COFFEELINT} --reporter=csv --file=%s ${SRC}'
+            % str(next(
+                (Path(direct)/'coffeelintrc').resolve()
+                for direct in ('', 'linting', '..', '../linting')
+                if (Path(direct)/'coffeelintrc').exists()
+            ))
+        )
+        for i in coffees:
+            bld(
+                source = [i],
+                rule        = cmd,
+                color       = 'BLUE',
+                cls_keyword = lambda _: 'CoffeeLint',
+                group  = 'coffeelint',
+            )
