80b10c7d4fa17d8edda66a157b3e3aab09b35f4d
Pol d'Avezac <pol.davezac@depixus.com>
2019-04-18 09:00:14 +0200

    adding c++ code coverage

--- _python/_pytest.py
+++ _python/_pytest.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 "All python-testing related details"
+def _run(cov, args):
+    from   importlib import import_module
+    import_module(cov).main(args)
+
 class PyTesting:
     "All python-testing related details"
-    TEST  = 'pytest'
-    OPTS  = ["--tb", "short"]
-    COV   = 'coverage.cmdline'
-    OMITS = ["--omit", 'tests/*.py,*waf*.py,*test*.py']
-    HTML  = "Coverage"
+    TEST    = 'pytest'
+    OPTS    = ["--tb", "short"]
+    COV     = 'coverage.cmdline'
+    OMITS   = ["--omit", 'tests/*.py,*waf*.py,*test*.py']
+    HTML    = "Coverage"
+    CAPTURE = "lcov --capture --directory ./ --output-file {output}"
+    REMOVE  = "lcov --remove {input} \"*/include/*\" --output-file {output}"
+    GENHTML = "genhtml {input} --output-directory {output}"
+    INDEX   = """
+        <!DOCTYPE HTML PUBLIC>
+        <html lang='en'>
+            <head>
+                <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+                <title>Code Coverage</title>
+            </head>
+            <body>
+                <iframe src="Cpp/index.html" width='100%' height='500px'></iframe>
+                <iframe src="Python/index.html" width='100%' height='500px'></iframe>
+            </body>
+        </html>
+        """
     @staticmethod
     def options(ctx):
         "add options"
@@ -51,26 +71,42 @@ class PyTesting:
         )
 
     @classmethod
-    def test(cls, _):
+    def test(cls, bld):
         "do unit tests"
         import os
         from   pathlib   import Path
         from   importlib import import_module
         os.chdir("build")
-        opt = _.options
+        opt = bld.options
         if opt.TEST_HEADLESS:
             os.environ['DPX_TEST_HEADLESS'] = 'True'
 
         junit = () if not opt.JUNIT_XML else ('--junit-xml', opt.JUNIT_XML)
-        cmd   = ["tests/", *opt.TEST_GROUP, *junit, *cls.OPTS]
+        cmd   = ["tests/test_peakcalling.py", *opt.TEST_GROUP, *junit, *cls.OPTS]
         if opt.PYTEST_V:
             cmd.append("-v")
         if not opt.TEST_COV:
             import_module(cls.TEST).cmdline.main(cmd)
-        else:
-            import_module(cls.COV).main(["run", *cls.OMITS, "-m", cls.TEST, *cmd])
-            Path(opt.TEST_COV).mkdir(parents = True, exist_ok = True)
-            import_module(cls.COV).main(["html", "-i", *cls.OMITS, "-d", opt.TEST_COV])
+            return
+
+        for i in Path(".").glob("./**/*.gcda"):
+            i.unlink()
+
+        import_module(cls.COV).main(["run", *cls.OMITS, "-m", cls.TEST, *cmd])
+
+    @classmethod
+    def html(cls, bld):
+        "create the html"
+        import os
+        from   pathlib   import Path
+        from   importlib import import_module
+        os.chdir("build")
+        opt = bld.options
+        Path(opt.TEST_COV).mkdir(parents = True, exist_ok = True)
+        out = opt.TEST_COV + ('/Python' if any(Path(".").glob("./**/*.gcda")) else '')
+        import_module(cls.COV).main(["html", "-i", *cls.OMITS, "-d", out])
+        if any(Path(".").glob("./**/*.gcda")):
+            cls.__lcov(bld)
 
     @classmethod
     def make(cls, locs):
@@ -81,5 +117,26 @@ class PyTesting:
 
         locs.update(
             options = options,
-            test    = cls.test
+            test    = cls.test,
+            html    = cls.html
         )
+
+    @classmethod
+    def __lcov(cls, bld):
+        "create lcov report"
+        cwd = "build"
+        opt = bld.options
+        bld.cmd_and_log(
+            cls.CAPTURE.format(output = "cppcoverage.info"),
+            cwd = cwd
+        )
+        bld.cmd_and_log(
+            cls.REMOVE.format(input  = "cppcoverage.info", output = "cppfiltered.info"),
+            cwd = cwd
+        )
+        bld.cmd_and_log(
+            cls.GENHTML.format(input  = "cppfiltered.info", output = opt.TEST_COV+"/Cpp"),
+            cwd = cwd
+        )
+        with open(opt.TEST_COV+"/index.html", "w", encoding = "utf-8") as stream:
+            print(cls.INDEX, file = stream)
